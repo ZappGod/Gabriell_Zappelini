e37fdc7cd3d632ff574e61a2d3fdd66c5911296f
ZappGod <GABRIELLSZAPP@gmail.com>
2023-11-09 12:44:16 -0300

    Começo

---
+++ js/cad.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const apelido = document.getElementById('apelido');
+const telefone = document.getElementById('telefone')
+const email = document.getElementById('email');
+var dados = JSON.parse(localStorage.getItem('dados')) || [];
+const key = new URLSearchParams(window.location.search).get('chave');
+
+function salvarDados()
+{
+    let novoNome = nome.value;
+    let novoApelido = apelido.value;
+    let novoTelefone = telefone.value;
+    let novoEmail = email.value;
+    if(key)
+    {
+        dados[key] = {nome: novoNome,apelido: novoApelido,telefone: novoTelefone,email: novoEmail};
+    }else{
+        dados.push({nome: novoNome,apelido: novoApelido,telefone: novoTelefone,email: novoEmail});
+    }
+
+    localStorage.setItem('dados',JSON.stringify(dados));
+    window.location.href= "./cadastro.html";
+}
+
+if(key)
+{
+    nome.value = dados[key].nome;
+    apelido.value = dados[key].apelido;
+    telefone.value = dados[key].telefone;
+    email.value = dados[key].email;
+}
---
+++ js/form.js
@@ -0,0 +1,28 @@
+const tabela = document.getElementById('tabela');
+var dados = JSON.parse(localStorage.getItem('dados')) || [];
+
+function exibirDados()
+{
+    dados.forEach((elemento, chave) =>
+    {
+        tabela.innerHTML += `
+            <tr>
+                <td>${elemento.nome}</td>
+                <td>${elemento.apelido}</td>
+                <td>${elemento.telefone}</td>
+                <td>${elemento.email}</td>
+                <td>
+                    <a href="../src/cadastro.html?chave=${chave}">Editar</a>
+                    <a href="#" onClick="remover(${chave})">Remover</a>
+                </td>
+            </tr>
+        `;
+    });
+}
+function remover(chave)
+{
+    dados.splice(chave, 1);
+    localStorage.setItem('dados', JSON.stringify(dados));
+    window.location.href= "./form.html";
+}
+exibirDados();
